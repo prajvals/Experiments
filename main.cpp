541ff0c2705d20a3e0361c98d558f947cc30746f
Prajval Singh <singhprajval91@gmail.com>
2020-05-25 19:24:29 +0530

    hey

--- main.cpp
+++ main.cpp
@@ -6,5 +6,7 @@ void main()
     cout << "this is for the git test";
     cout << "ths is the change I want to make";
     cout << "hey";
-    cout<<"this is a beautiful world";
+    cout << "this is a beautiful world";
+    cout << " I will make it bro";
+    cout << "hey";
 }
